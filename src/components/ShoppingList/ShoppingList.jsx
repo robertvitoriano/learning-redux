b5fee4edb6e175ebb5be94b7b017d5b7a1885d6b
robertvitoriano <robertvitorianodev@gmail.com>
2020-10-27 07:48:08 -0300

    modularing selectors

--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import { connect,useSelector } from "react-redux";
 import { Wrapper, Title, Array } from './ShoppingList.styles'
 import Checkbox from '../../shared/Checkbox'
+import { selectAllProducts } from '../../store/Products/Products.selectors';
 
 function ShoppingList ({ title, products, onToggle,productsFromRedux }) {
   console.log("sem hooks",productsFromRedux)
   const reduxProducts = useSelector(state=>state.products)
   console.log("Com hooks:",reduxProducts)
+  selectAllProducts
+  // console.log("select modularizado:", useSelector(selectAllProducts));
+
   return <Wrapper>
     <Title>
       { title }:
